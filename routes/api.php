4be9b8529ca8c9c2dfe02fa90e983d03c2fd2262
Khafid224 <abdulhafid1230@gmail.com>
2024-07-30 11:18:16 +0700

    Membuat Controller & route API login

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,18 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:api');
+//group route with prefix "admin"
+Route::prefix('admin')->group(function () {
+
+    //route login
+    Route::post('/login', App\Http\Controllers\Api\Admin\LoginController::class, ['as' => 'admin']);
+
+    //group route with middleware "auth:api"
+    Route::group(['middleware' => 'auth:api'], function () {
+
+        //route user logged in
+        Route::get('/user', function (Request $request) {
+            return $request->user();
+        })->name('user');
+    });
+});
