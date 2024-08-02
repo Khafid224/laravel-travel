23162da9aac7e4bc21c0dd78f377258e23013233
Khafid224 <abdulhafid1230@gmail.com>
2024-08-02 20:47:59 +0700

    Default Parameter

--- hello-world.js
+++ hello-world.js
@@ -1,3 +1,28 @@
-const text = 'Learning ES6'
+//coba
+const text = 'Learning ES6 -> coba'
 
 console.log(text);
+
+// Variable didalam ES6
+var name = 'Abdul Hafid';
+
+if(true) {
+	var name = 'Hafid -> Variable ES6';
+	console.log(name); 
+}
+
+console.log(name); 
+
+// Default Parameter
+function hello(message = 'Hello World! -> default parameter') {
+
+	console.log(message);
+
+}
+
+//panggil function tanpa parameter
+console.log(hello()); // <-- output "Hello World!"
+
+//panggil function dengan parameter
+console.log(hello('Belajar ES6 -> default parameter')); //<-- output "Belajar ES6"
+
